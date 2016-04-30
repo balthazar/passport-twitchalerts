9915c4bd4ab1c3ca7c3d3deba30148cbcfa8d4b3
Aperçu <bgronon@gmail.com>
2016-04-30 12:20:52 +0200

    fix(core): change urls

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ const OAuth2Strategy = require('passport-oauth').OAuth2Strategy
 
 function Strategy(options, verify) {
   options = options || {}
-  options.authorizationURL = options.authorizationURL || 'https://twitchalerts.com/oauth/authorize'
-  options.tokenURL = options.tokenURL || 'https://twitchalerts.com/api/v1.0/token'
+  options.authorizationURL = options.authorizationURL || 'https://www.twitchalerts.com/api/v1.0/authorize'
+  options.tokenURL = options.tokenURL || 'https://www.twitchalerts.com/api/v1.0/token'
 
   OAuth2Strategy.call(this, options, verify)
   this.name = 'twitchalerts'
-
-  // this._oauth2.setAuthMethod('OAuth')
-  // this._oauth2.useAuthorizationHeaderforGET(true)
 }
 
 util.inherits(Strategy, OAuth2Strategy)
