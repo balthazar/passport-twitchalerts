0ce9e7dbd38135ec6509f01b3030f6b381a4decc
Balthazar Gronon <bgronon@gmail.com>
2017-04-14 20:52:17 -0700

    feat(core): add userProfile retrival

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const util = require('util')
 const OAuth2Strategy = require('passport-oauth').OAuth2Strategy
+const InternalOAuthError = require('passport-oauth').InternalOAuthError
 
-function Strategy(options, verify) {
+function Strategy (options, verify) {
   options = options || {}
   options.authorizationURL = options.authorizationURL || 'https://streamlabs.com/api/v1.0/authorize'
   options.tokenURL = options.tokenURL || 'https://streamlabs.com/api/v1.0/token'
@@ -12,5 +13,22 @@ function Strategy(options, verify) {
 }
 
 util.inherits(Strategy, OAuth2Strategy)
+
+Strategy.prototype.userProfile = function (accessToken, done) {
+  this._oauth2.get('https://streamlabs.com/api/v1.0/user', accessToken, function (err, body) {
+    if (err) { return done(new InternalOAuthError('Cannot fetch user', err)) }
+    try {
+
+      const json = JSON.parse(body)
+      const profile = Object.assign({
+        provider: 'twitchalerts',
+      }, json.twitch)
+
+      done(null, profile)
+    } catch (e) {
+      done(e)
+    }
+  })
+}
 
 exports.Strategy = Strategy
